4c98057f1bf8d20980550b71c7f95c8bb643223d
arif286 <71484413+arif286@users.noreply.github.com>
2021-11-09 15:49:24 +0600

    add spinner in Food component

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -3,15 +3,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Tab, Tabs } from "react-bootstrap";
 import { useHistory } from "react-router";
+import RingLoader from "react-spinners/RingLoader";
 import "./Food.css";
 import FoodItem from "./FoodItem";
 
 const Food = () => {
   const [foods, setFoods] = useState([]);
+  const [loading, setLoading] =useState(true)
   useEffect(() => {
     axios
       .get("https://red-onion-server-121.herokuapp.com/foods")
-      .then((res) => setFoods(res.data))
+      .then((res) => {
+        if (res.data) {
+          setFoods(res.data);
+          setLoading(false)
+        }
+
+      })
       .catch((err) => console.log(err));
   }, []);
   let history = useHistory()
@@ -22,56 +30,61 @@ const Food = () => {
   const dinner = foods.filter((item) => item.category === 'dinner');
   const lunch = foods.filter((item) => item.category === "lunch");
   const breakfast = foods.filter((item) => item.category === "breakfast");
-  console.log(dinner);
 
   return (
     <>
       <div className="container mt-4">
-        <Tabs
-          className="justify-content-center food-items"
-          defaultActiveKey="breakfast"
-          id="uncontrolled-tab-example"
-        >
-          <Tab eventKey="breakfast" title="Breakfast">
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-              {breakfast.map((food) => (
-                <FoodItem
-                  handleFood={() => {
-                    handleFood(food._id);
-                  }}
-                  food={food}
-                  key={food.id}
-                ></FoodItem>
-              ))}
-            </div>
-          </Tab>
-          <Tab eventKey="dinner" title="Dinner">
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-              {dinner.map((food) => (
-                <FoodItem
-                  handleFood={() => {
-                    handleFood(food._id);
-                  }}
-                  food={food}
-                  key={food.id}
-                ></FoodItem>
-              ))}
-            </div>
-          </Tab>
-          <Tab eventKey="lunch" title="Lunch">
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-              {lunch.map((food) => (
-                <FoodItem
-                  handleFood={() => {
-                    handleFood(food._id);
-                  }}
-                  food={food}
-                  key={food.id}
-                ></FoodItem>
-              ))}
-            </div>
-          </Tab>
-        </Tabs>
+        {foods.length === 0 ? (
+          <div className="spinner">
+            <RingLoader color="red" loading={loading} size={80} />
+          </div>
+        ) : (
+          <Tabs
+            className="justify-content-center food-items"
+            defaultActiveKey="breakfast"
+            id="uncontrolled-tab-example"
+          >
+            <Tab eventKey="breakfast" title="Breakfast">
+              <div className="row row-cols-1 row-cols-md-3 g-4">
+                {breakfast.map((food) => (
+                  <FoodItem
+                    handleFood={() => {
+                      handleFood(food._id);
+                    }}
+                    food={food}
+                    key={food.id}
+                  ></FoodItem>
+                ))}
+              </div>
+            </Tab>
+            <Tab eventKey="dinner" title="Dinner">
+              <div className="row row-cols-1 row-cols-md-3 g-4">
+                {dinner.map((food) => (
+                  <FoodItem
+                    handleFood={() => {
+                      handleFood(food._id);
+                    }}
+                    food={food}
+                    key={food.id}
+                  ></FoodItem>
+                ))}
+              </div>
+            </Tab>
+            <Tab eventKey="lunch" title="Lunch">
+              <div className="row row-cols-1 row-cols-md-3 g-4">
+                {lunch.map((food) => (
+                  <FoodItem
+                    handleFood={() => {
+                      handleFood(food._id);
+                    }}
+                    food={food}
+                    key={food.id}
+                  ></FoodItem>
+                ))}
+              </div>
+            </Tab>
+          </Tabs>
+        )}
       </div>
     </>
   );
--- src/components/Food/FoodItem.js
+++ src/components/Food/FoodItem.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-const FoodItem = ({food, handleFood}) => {
-
-  const { name, picture, description, price} = food;
-
+const FoodItem = ({food:{name,picture,description,price}, handleFood}) => {
     return (
       <>
         <div className="col py-3">
@@ -13,13 +10,13 @@ const FoodItem = ({food, handleFood}) => {
             }}
             className="card h-100 food-card"
           >
-            <img src={picture} className="card-img-top fluid" alt="..." />
+              <img src={picture} className="card-img-top fluid" alt={name} />
             <div className="card-body">
               <h5 className="card-title">{name}</h5>
               <p className="card-text">{description}</p>
               <div className="card-body d-flex justify-content-between">
-                <p className=''>price: ${price}</p>
-                <button className='btn btn-danger'>View Details</button>
+                <p className="">price: ${price}</p>
+                <button className="btn btn-danger">View Details</button>
               </div>
             </div>
           </div>
